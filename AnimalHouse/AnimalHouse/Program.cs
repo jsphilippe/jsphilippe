df5e78bc9791a88793c6ac1d27c73e823dda1464
jsphilippe <157438908+jsphilippe@users.noreply.github.com>
2024-03-19 00:04:54 -0400

    AnimalHouse Upload
    C# console app using the Dictionary Factory Pattern

---
+++ AnimalHouse/AnimalHouse/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using Ninject;
+
+public enum Animals
+{
+    Dog,
+    Cat,
+    Parrot
+}
+
+interface IAnimal
+{
+    void Speak();
+}
+
+class Dog : IAnimal
+{
+    public void Speak()
+    {
+        Console.WriteLine("Bow-wow!");
+    }
+}
+
+class Cat : IAnimal
+{
+    public void Speak()
+    {
+        Console.WriteLine("Meow!");
+    }
+}
+
+class Parrot : IAnimal
+{
+    public void Speak()
+    {
+        Console.WriteLine("Polly wants a cracker!");
+    }
+}
+
+class AnimalFactory
+{
+    private static IKernel ninject_kernel;
+    private static Dictionary<Animals, Func<IAnimal>> animalMaker;
+    static AnimalFactory()
+    {
+        ninject_kernel = new StandardKernel();
+        ninject_kernel.Bind<IAnimal>().To<Dog>().Named(Animals.Dog.ToString());
+        ninject_kernel.Bind<IAnimal>().To<Cat>().Named(Animals.Cat.ToString());
+        ninject_kernel.Bind<IAnimal>().To<Parrot>().Named(Animals.Parrot.ToString());
+        animalMaker = new Dictionary<Animals, Func<IAnimal>>
+        {
+            {
+                Animals.Dog,
+                () => ninject_kernel.Get<IAnimal>(Animals.Dog.ToString())
+            },
+            {
+                Animals.Cat,
+                () => ninject_kernel.Get<IAnimal>(Animals.Cat.ToString())
+            },
+            {
+                Animals.Parrot,
+                () => ninject_kernel.Get<IAnimal>(Animals.Parrot.ToString())
+            }
+        };
+    }
+
+    public static IAnimal MakeAnimal(Animals animal)
+    {
+        if (animalMaker.TryGetValue(animal, out var creator))
+        {
+            return creator();
+        }
+        else
+        {
+            throw new ArgumentException("Invalid animal type", nameof(animal));
+        }
+    }
+}
+
+class AnimalHouse
+{
+    static void Main()
+    {
+        IAnimal dog = AnimalFactory.MakeAnimal(Animals.Dog);
+        IAnimal cat = AnimalFactory.MakeAnimal(Animals.Cat);
+        IAnimal parrot = AnimalFactory.MakeAnimal(Animals.Parrot);
+        dog.Speak();
+        cat.Speak();
+        parrot.Speak();
+    }
+}
